a48c3d558edf273b9b4237df43cdd44a9aa96891
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-11-03 13:24:17 +0700

    feat: add stochastic hc

--- hill_climbing.py
+++ hill_climbing.py
@@ -193,4 +193,74 @@ class HillClimbingSolver(CubeSolver):
 
         return all_state[-1][0], all_state[-1][1]
     
-    
+    def stochastic_hill_climbing(self, max_iteration = 100): 
+        current_state = self.generate_random_initial_state()
+        initial_state = current_state.copy()
+        current_value = self.calculate_objective(current_state)
+        iteration = 0
+        iterations = []
+        objective_values = []
+        
+        start_time = datetime.now()
+
+        plt.ion() 
+        fig= plt.figure(figsize=(10, 8))
+        gs = fig.add_gridspec(nrows=2, ncols=1, height_ratios=[1, 2])
+
+        ax1 = fig.add_subplot(gs[0])
+        ax1.set_xlabel('Iteration')
+        ax1.set_ylabel('Objective Value')
+        
+        ax1.set_title('Stochastic Hill Climbing Objective Value over Iterations')
+
+        gs2 = gs[1].subgridspec(1, 5)
+        cube_axes = [fig.add_subplot(gs2[0, i]) for i in range(5)]
+
+        self.visualize_state(current_state,axes=cube_axes)
+        plt.pause(0.1)
+
+        while iteration <= max_iteration:
+            iterations.append(iteration)
+            objective_values.append(current_value)
+             
+            ax1.plot(iterations, objective_values, color='blue')
+
+            for ax in cube_axes:
+                ax.clear()
+            self.visualize_state(current_state, axes=cube_axes)
+            plt.pause(0.1)
+            
+            neighbors= self.generate_neighbors(current_state)
+            random_neighbor = random.choice(neighbors)
+
+            neighbor_value = self.calculate_objective(random_neighbor)
+            if neighbor_value > current_value:
+                current_state = random_neighbor
+                current_value = neighbor_value
+
+            iteration += 1
+
+        plt.ioff()
+        fig2 = plt.figure(figsize=(12, 6))
+        fig2.suptitle('Initial and Final States')
+
+        gs_init_final = fig2.add_gridspec(2, 5)
+
+        axes_initial = [fig2.add_subplot(gs_init_final[0, i]) for i in range(5)]
+        axes_final = [fig2.add_subplot(gs_init_final[1, i]) for i in range(5)]
+
+        self.visualize_state(initial_state, axes=axes_initial)
+        self.visualize_state(current_state, axes=axes_final)
+
+        end_time = datetime.now()
+        duration = (end_time - start_time).total_seconds() * 1000
+
+        fig2.text(0.5, 0.05, f'Final Objective Value: {current_value} Duration: {duration} ms', ha='center', fontsize=10)
+
+        for ax in axes_initial:
+            ax.set_title('Initial State', fontsize=8)
+        for ax in axes_final:
+            ax.set_title('Final State', fontsize=8)
+
+        plt.show()
+        return current_state, iteration
